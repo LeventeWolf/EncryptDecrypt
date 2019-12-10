11001686c5ed2109798aebc460caf71fcbbb18e1
Levente Wolf <wlevi2002@gmail.com>
2019-12-10 21:16:54 +0100

    Getting files from resource folder.

--- src/encryptdecrypt/plainText/PlainTextFromFile.java
+++ src/encryptdecrypt/plainText/PlainTextFromFile.java
@@ -4,17 +4,15 @@ import encryptdecrypt.IOHandlers.ArgumentHandler;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.net.URL;
 import java.util.Scanner;
 
 public class PlainTextFromFile implements PlainTextMethod {
-    @Override
-    public String getPlainText(String[] args) {
-        return getCipherTextFromFile(args);
-    }
-
     private static String getCipherTextFromFile(String[] args) {
         ArgumentHandler argHandler = new ArgumentHandler(args);
-        File file = new File(argHandler.getInputFileName());
+        PlainTextFromFile main = new PlainTextFromFile();
+        File file = main.getFileFromResources(argHandler.getInputFileName());
+
         String cipherText = "";
         try (Scanner sc = new Scanner(file)) {
             cipherText = sc.nextLine();
@@ -23,5 +21,23 @@ public class PlainTextFromFile implements PlainTextMethod {
         }
 
         return cipherText;
+    }
+
+    @Override
+    public String getPlainText(String[] args) {
+        return getCipherTextFromFile(args);
+    }
+
+    private File getFileFromResources(String fileName) {
+
+        ClassLoader classLoader = getClass().getClassLoader();
+
+        URL resource = classLoader.getResource(fileName);
+        if (resource == null) {
+            throw new IllegalArgumentException("file is not found!");
+        } else {
+            return new File(resource.getFile());
+        }
+
     }
 }
