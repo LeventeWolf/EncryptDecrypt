7ddcc3b6fd0510662dd7b02ecc1b9613d771a56b
Levente Wolf <wlevi2002@gmail.com>
2019-12-14 17:10:33 +0100

    Add HashMap for storing arguments && Rewrite getter methods in ArgumentHandler

--- src/encryptdecrypt/IOHandlers/ArgumentHandler.java
+++ src/encryptdecrypt/IOHandlers/ArgumentHandler.java
@@ -1,90 +1,82 @@
 package encryptdecrypt.IOHandlers;
 
+import encryptdecrypt.exceptions.ArgumentNotFoundException;
+
+import java.util.HashMap;
+
 public class ArgumentHandler {
-    //TODO use a HashMap for arguments:
-    //key: -data value: "Welcome to hyperskill!
+    //TODO handle wrong "-mode"-s and missing arguments "-out=not given"
     private String[] args;
+    private HashMap<String, String> argumentMap = new HashMap<>();
 
     public ArgumentHandler(String[] args) {
         this.args = args;
+        setArgumentMap();
     }
 
+    public void setArgumentMap() {
+        String mode;
+        String nextArg;
+        for (int i = 0; i < args.length - 1; i += 2) {
+            mode = args[i];
+            nextArg = args[i + 1];
+            argumentMap.put(mode, nextArg);
+        }
+        System.out.println(argumentMap.toString());
+    }
+
+    public HashMap<String, String> getArgumentMap() {
+        return argumentMap;
+    }
+    
     public String getData() {
-        for (int i = 0; i < args.length; i++)
-            if (args[i].equals("-data")) {
-                return args[i + 1];
-            }
-        return "";
+        return argumentMap.get("-data");
     }
 
-    public int getKey() {
-        for (int i = 0; i < args.length; i++)
-            if (args[i].equals("-key"))
-                return Integer.parseInt(args[i + 1]);
-        return 0;
+    public int getKey() throws NumberFormatException {
+        String key = argumentMap.get("-key");
+
+        try {
+            return Integer.parseInt(key);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid key: \"" + key + "\"");
+            return 0;
+        }
     }
 
     public String getMode() {
-        for (int i = 0; i < args.length; i++)
-            if (args[i].equals("-mode")) {
-                return args[i + 1];
-            }
-        return "enc";
+        return argumentMap.get("-mode");
     }
 
     //TODO write unit test for getInPutMode
     public String getInPutMode() {
-        boolean wasData = false;
-        boolean wasInPutFileName = false;
-
-        for (String arg : args) {
-            if (arg.equals("-data")) wasData = true;
-            else if (arg.equals("-in")) wasInPutFileName = true;
-        }
-        if (wasData) {
+        if (argumentMap.get("-data") != null) {
             return "console";
-        } else if (wasInPutFileName) {
+        } else if (argumentMap.get("-in") != null) {
             return "file";
         } else {
-            //TODO: can throw ArgumentNullException
-            //Hyperskill implementation = return "";
-            return "";
+            throw new ArgumentNotFoundException("Argument not found: -data or -in");
         }
     }
 
     //TODO write unit test for getOutPutMode
     public String getOutPutMode() {
-        boolean wasOutPut = false;
-
-        for (String arg : args) {
-            if (arg.equals("-out")) {
-                wasOutPut = true;
-                break;
-            }
+        if (argumentMap.get("-out") != null) {
+            return "file";
+        } else {
+            return "console";
         }
-
-        if (wasOutPut) return "file";
-        else return "console";
     }
 
     public String getInputFileName() {
-        for (int i = 0; i < args.length; i++)
-            if (args[i].equals("-in"))
-                return args[i + 1];
-        return "";
+        return argumentMap.get("-in");
     }
 
     public String getOutPutFileName() {
-        for (int i = 0; i < args.length; i++)
-            if (args[i].equals("-out"))
-                return args[i + 1];
-        return "";
+        return argumentMap.get("-out");
     }
 
     public String getAlgorithm() {
-        for (int i = 0; i < args.length; i++)
-            if (args[i].equals("-alg"))
-                return args[i + 1];
-        return "";
+        return argumentMap.get("-alg");
     }
 }
---
+++ src/encryptdecrypt/exceptions/ArgumentNotFoundException.java
@@ -0,0 +1,7 @@
+package encryptdecrypt.exceptions;
+
+public class ArgumentNotFoundException extends Exception {
+    public ArgumentNotFoundException(String message) {
+        super(message);
+    }
+}
